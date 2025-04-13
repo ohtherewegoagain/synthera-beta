3af2b45bd7205cacc77c0c3a5807483b22b257c7
ohtherewegoagain <pooja892892@gmail.com>
2025-04-14 00:08:30 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import SyntheraWebsite from './SyntheraWebsite.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SyntheraWebsite />
-  </React.StrictMode>
-);
-import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<SyntheraWebsite />);
